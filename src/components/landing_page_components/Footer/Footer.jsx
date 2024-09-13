b1e348b1988358d1438c39d9128368bbbbaa966c
benjaminpaniagua <benjaminjosepaniaguarojas@gmail.com>
2024-09-12 21:14:03 -0600

    feat: adding animations in the footer.

--- src/components/landing_page_components/Footer/Footer.jsx
+++ src/components/landing_page_components/Footer/Footer.jsx
@@ -7,32 +7,32 @@ const Footer = () => {
       <div className="max-w-screen-xl mx-auto gap-6 text-center flex items-start md:grid md:grid-cols-1 justify-between">
         <div className="flex gap-64 md:flex-col md:gap-6">
           
-        <div className="flex flex-col space-y-4 md:items-center items-start">
-          <a href="#" className="text-clr-black font-medium">
-            Servicios
-          </a>
-          <a href="#" className="text-clr-black font-medium">
-            Blog
-          </a>
-          <a href="#" className="text-clr-black font-medium">
-            Contacto
-          </a>
-        </div>
-        
-        <div className="flex flex-col space-y-4 md:items-center items-start">
-          <a href="#" className="text-clr-black font-medium">
-            Política de Privacidad
-          </a>
-          <a href="#" className="text-clr-black font-medium">
-            Preguntas Frecuentes
-          </a>
-          <a href="#" className="text-clr-black font-medium">
-            Términos y Condiciones
-          </a>
-        </div>
+          <div className="flex flex-col space-y-4 md:items-center items-start">
+            <a href="#" className="text-clr-black font-medium underline-animated hover:text-clr-blue-hover">
+              Servicios
+            </a>
+            <a href="#" className="text-clr-black font-medium underline-animated hover:text-clr-blue-hover">
+              Blog
+            </a>
+            <a href="#" className="text-clr-black font-medium underline-animated hover:text-clr-blue-hover">
+              Contacto
+            </a>
+          </div>
+          
+          <div className="flex flex-col space-y-4 md:items-center items-start">
+            <a href="#" className="text-clr-black font-medium underline-animated hover:text-clr-blue-hover">
+              Política de Privacidad
+            </a>
+            <a href="#" className="text-clr-black font-medium underline-animated hover:text-clr-blue-hover">
+              Preguntas Frecuentes
+            </a>
+            <a href="#" className="text-clr-black font-medium underline-animated hover:text-clr-blue-hover">
+              Términos y Condiciones
+            </a>
+          </div>
         </div>
 
-        <div className="flex flex-col  md:items-center space-y-4 items-end">
+        <div className="flex flex-col md:items-center space-y-4 items-end">
           <div className="flex space-x-4">
             <a href="#" className="text-clr-black text-xl">
               <FaInstagram />
