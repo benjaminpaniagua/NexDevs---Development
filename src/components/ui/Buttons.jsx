ed2d5c9335ec2162913b56b950b8b2273fe72306
LuisCarlosSolis <168393621+LuisCarlosSolis@users.noreply.github.com>
2024-09-16 15:47:19 -0600

    feat: Añadidos nuevos botones maestros

--- src/components/ui/Buttons.jsx
+++ src/components/ui/Buttons.jsx
@@ -1,15 +1,47 @@
-export function SecondaryButton({ text, width, paddingX, onClick }) {
-    return (
-        <>
-            <button onClick={onClick} className={`text-black hover:text-white rounded-lg bg-clr-white hover:bg-clr-black border-2 border-clr-black py-2 transition-all duration-500 ${width} ${paddingX}`}>{text}</button>
-        </>
-    );
+export function MainButton({ text, width, paddingX, onClick, disabled }) {
+    if (disabled) {
+        return (
+            <>
+                <button onClick={onClick} className={`text-clr-white bg-clr-grey rounded-lg border-2 border-clr-gray transition-all duration-500 py-2 brightness-[1.5] ${paddingX} ${width}`}>{text}</button>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <button onClick={onClick} className={`text-clr-white hover:text-clr-green-dark bg-clr-black rounded-lg border-2 border-clr-black transition-all duration-500 py-2 ${paddingX} ${width}`}>{text}</button>
+            </>
+        );
+    }
 }
 
-export function GreenButton({ text, width }) {
-    return (
-        <>
-            <button className={`text-black hover:text-white rounded-lg bg-clr-green-light hover:bg-clr-green-dark border-2 border-clr-green-light hover:border-clr-green-dark transition-all py-2 duration-500 ${width}`}>{text}</button>
-        </>
-    );
+export function SecondaryButton({ text, width, paddingX, onClick, disabled }) {
+    if (disabled) {
+        return (
+            <>
+                <button onClick={onClick} className={`text-clr-black bg-clr-green-light rounded-lg border-2 border-clr-green-light transition-all duration-500 py-2 saturate-50 ${paddingX} ${width}`}>{text}</button>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <button onClick={onClick} className={`text-clr-black hover:text-clr-white bg-clr-green-light hover:bg-clr-green-dark rounded-lg border-2 border-clr-green-light hover:border-clr-green-dark transition-all duration-500 py-2 ${paddingX} ${width}`}>{text}</button>
+            </>
+        );
+    }
+}
+
+export function SecondaryButtonOutline({ text, width, paddingX, onClick, disabled }) {
+    if (disabled) {
+        return (
+            <>
+                <button onClick={onClick} className={`text-clr-black bg-clr-white rounded-lg border-2 border-clr-black transition-all duration-500 py-2 opacity-50 ${paddingX} ${width}`}>{text}</button>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <button onClick={onClick} className={`text-clr-black hover:text-clr-white bg-clr-white hover:bg-clr-black rounded-lg border-2 border-clr-black transition-all duration-500 py-2 ${paddingX} ${width}`}>{text}</button>
+            </>
+        );
+    }
 }
