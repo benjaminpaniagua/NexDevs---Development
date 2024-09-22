5830ab08e40bee9bc1904e1903d5a0574033eafa
freddy74 <freddygarro74@gmail.com>
2024-09-22 12:55:18 -0600

    refactor: cambio hacia un fetch dinámico para mostrar las categorías

--- src/components/pages/Categories_Page.jsx
+++ src/components/pages/Categories_Page.jsx
@@ -2,113 +2,34 @@ import CardCategories from "../ui/Cards/CardCategories";
 import SecondaryButton from "../ui/SecondaryButton";
 import { useState, useEffect } from "react";
 import { Search_Input } from "../ui/Search_Input";
-
-const categories = [
-  {
-    title: "Category 1",
-    imageUrl: "./images/categories/aire-acondicionado.jpg",
-  },
-  {
-    title: "Category 2",
-    imageUrl: "./images/categories/aire-acondicionado.jpg",
-  },
-  {
-    title: "Category 3",
-    imageUrl: "./images/categories/aire-acondicionado.jpg",
-  },
-  {
-    title: "Category 4",
-    imageUrl: "./images/categories/aire-acondicionado.jpg",
-  },
-  {
-    title: "Category 5",
-    imageUrl: "./images/categories/aire-acondicionado.jpg",
-  },
-  {
-    title: "Category 6",
-    imageUrl: "./images/categories/aire-acondicionado.jpg",
-  },
-  {
-    title: "Category 7",
-    imageUrl: "./images/categories/aire-acondicionado.jpg",
-  },
-  {
-    title: "Category 8",
-    imageUrl: "./images/categories/aire-acondicionado.jpg",
-  },
-  {
-    title: "Category 9",
-    imageUrl: "./images/categories/aire-acondicionado.jpg",
-  },
-  {
-    title: "Category 10",
-    imageUrl: "./images/categories/aire-acondicionado.jpg",
-  },
-  {
-    title: "Category 11",
-    imageUrl: "./images/categories/aire-acondicionado.jpg",
-  },
-  {
-    title: "Category 12",
-    imageUrl: "./images/categories/aire-acondicionado.jpg",
-  },
-  {
-    title: "Category 13",
-    imageUrl: "./images/categories/aire-acondicionado.jpg",
-  },
-  {
-    title: "Category 14",
-    imageUrl: "./images/categories/aire-acondicionado.jpg",
-  },
-  {
-    title: "Category 15",
-    imageUrl: "./images/categories/aire-acondicionado.jpg",
-  },
-];
+import { useFetchCategories } from "../../hooks/useFetchCategories";
 
 const Categories_Page = () => {
+  const { categories, loading } = useFetchCategories();
   const [categoriesToShow, setCategoriesToShow] = useState(categories);
 
-  useEffect(() => {
-    const updateCategories = () => {
-      if (window.innerWidth <= 768) {
-        setCategoriesToShow(categories);
-      } else {
-        setCategoriesToShow(categories);
-      }
-    };
-
-    updateCategories();
-    window.addEventListener("resize", updateCategories);
-
-    return () => window.removeEventListener("resize", updateCategories);
-  }, []);
-
-  const handleCardClick = (title) => {
-    console.log(`Categoría seleccionada: ${title}`);
-  };
-
   return (
-    <div className="mt-10 grid md:p-4 mx-auto max-w-[100rem] min-w-[10rem]">
+    <div className="h-auto mt-10 grid md:p-4 mx-auto max-w-[100rem] min-w-[10rem]">
       <div className="flex mt-20 mb-4 items-center justify-between">
         <h2 className="font-clash">Categorías</h2>
         <Search_Input />
       </div>
-      <div className="grid gap-10">
-        <div className="grid grid-cols-auto-300 sm:grid-cols-auto-250 md:grid-cols-auto-300 lg:grid-cols-auto-350 gap-4">
-          {categoriesToShow.map((category, index) => (
-            <CardCategories
-              key={index}
-              title={category.title}
-              imageUrl={category.imageUrl}
-              onClick={() => handleCardClick(category.title)}
-            />
-          ))}
+      {loading ? (
+        <h3 className="text-center">Cargando categorias...</h3>
+      ) : (
+        <div className="grid gap-10">
+          <div className="grid grid-cols-auto-300 sm:grid-cols-auto-250 md:grid-cols-auto-300 lg:grid-cols-auto-350 gap-4">
+            {categories.map((category, index) => (
+              <CardCategories
+                key={category.categoryId}
+                title={category.categoryName}
+                imageUrl="/images/categories/aire-acondicionado.jpg"
+                onClick={() => handleCardClick(category.categoryName)}
+              />
+            ))}
+          </div>
         </div>
-        {/* <div className="flex justify-center mb-10">
-          <SecondaryButton text="Ver Más" sizeX="S" sizeY="S" />
-        </div> */}
-      </div>
+      )}
     </div>
   );
 };
