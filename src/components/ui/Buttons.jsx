0e992e47fc132cba24113e18aca954ab1de941ad
freddy74 <freddygarro74@gmail.com>
2024-10-01 15:59:41 -0600

    refactor: Update Icons component with new SVG icons

--- src/components/ui/Buttons.jsx
+++ src/components/ui/Buttons.jsx
@@ -1,106 +1,246 @@
 import PropTypes from "prop-types";
-export function MainButton({ text, extraStyles, onClick, disabled, type, id }) {
-    if (disabled) {
-        return (
-            <>
-                <button className={`text-clr-white bg-clr-grey rounded-lg border-2 border-clr-grey transition-all duration-300 brightness-[1.5] cursor-default ${extraStyles}`}>{text}</button>
-            </>
-        );
-    } else {
-        return (
-            <>
-                <button id={id} type={type} onClick={onClick} className={`text-clr-white hover:text-clr-green-dark bg-clr-black rounded-lg border-2 border-clr-black transition-all duration-300 ${extraStyles}`}>{text}</button>
-            </>
-        );
-    }
+
+export function MainButton({
+  text,
+  asLink,
+  extraStyles,
+  onClick,
+  disabled,
+  type,
+  id,
+  link
+}) {
+  const baseStyles =
+    "text-clr-white font-montserrat font-regular text-center rounded-lg border-2 transition-all duration-300";
+
+  if (disabled) {
+    return (
+      <button
+        className={`${baseStyles} bg-clr-grey border-clr-grey brightness-[1.5] cursor-default ${extraStyles}`}
+      >
+        {text}
+      </button>
+    );
+  } else if (asLink) {
+    return (
+      <a
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={`${baseStyles} bg-clr-black border-clr-black hover:text-clr-green-dark ${extraStyles}`}
+      >
+        {text}
+      </a>
+    );
+  } else {
+    return (
+      <button
+        id={id}
+        type={type}
+        onClick={onClick}
+        className={`${baseStyles} bg-clr-black border-clr-black hover:text-clr-green-dark ${extraStyles}`}
+      >
+        {text}
+      </button>
+    );
+  }
 }
 
-export function SecondaryButton({ text, extraStyles, onClick, disabled, type, id }) {
-    if (disabled) {
-        return (
-            <>
-                <button className={`text-clr-black bg-clr-green-light rounded-lg border-2 border-clr-green-light transition-all duration-300 saturate-50 cursor-default ${extraStyles}`}>{text}</button>
-            </>
-        );
-    } else {
-        return (
-            <>
-                <button id={id} type={type} onClick={onClick} className={`text-clr-black hover:text-clr-white bg-clr-green-light hover:bg-clr-green-dark rounded-lg border-2 border-clr-green-light hover:border-clr-green-dark transition-all duration-300 ${extraStyles}`}>{text}</button>
-            </>
-        );
-    }
+export function SecondaryButton({
+  text,
+  asLink,
+  extraStyles,
+  onClick,
+  disabled,
+  type,
+  id,
+  link
+}) {
+  const baseStyles =
+    "text-clr-black rounded-lg border-2 transition-all duration-300";
+
+  if (disabled) {
+    return (
+      <button
+        className={`${baseStyles} bg-clr-green-light border-clr-green-light saturate-50 cursor-default ${extraStyles}`}
+      >
+        {text}
+      </button>
+    );
+  } else if (asLink) {
+    return (
+      <a
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={`${baseStyles} bg-clr-green-light border-clr-green-light hover:bg-clr-green-dark ${extraStyles}`}
+      >
+        {text}
+      </a>
+    );
+  } else {
+    return (
+      <button
+        id={id}
+        type={type}
+        onClick={onClick}
+        className={`${baseStyles} bg-clr-green-light hover:bg-clr-green-dark rounded-lg border-2 border-clr-green-light hover:border-clr-green-dark transition-all duration-300 ${extraStyles}`}
+      >
+        {text}
+      </button>
+    );
+  }
 }
 
-export function SecondaryButtonOutline({ text, extraStyles, onClick, disabled, type, id }) {
-    if (disabled) {
-        return (
-            <>
-                <button className={`text-clr-black bg-clr-white rounded-lg border-2 border-clr-black transition-all duration-300 opacity-50 cursor-default ${extraStyles}`}>{text}</button>
-            </>
-        );
-    } else {
-        return (
-            <>
-                <button id={id} type={type} onClick={onClick} className={`text-clr-black hover:text-clr-white bg-clr-white hover:bg-clr-black rounded-lg border-2 border-clr-black transition-all duration-300 ${extraStyles}`}>{text}</button>
-            </>
-        );
-    }
+export function SecondaryButtonOutline({
+  text,
+  asLink,
+  extraStyles,
+  onClick,
+  disabled,
+  type,
+  id,
+  link
+}) {
+  const baseStyles =
+    "text-clr-black rounded-lg border-2 transition-all duration-300";
+
+  if (disabled) {
+    return (
+      <button
+        className={`${baseStyles} bg-clr-white border-clr-black opacity-50 cursor-default ${extraStyles}`}
+      >
+        {text}
+      </button>
+    );
+  } else if (asLink) {
+    return (
+      <a
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={`${baseStyles} bg-clr-white border-clr-black hover:bg-clr-black hover:text-clr-white ${extraStyles}`}
+      >
+        {text}
+      </a>
+    );
+  } else {
+    return (
+      <button
+        id={id}
+        type={type}
+        onClick={onClick}
+        className={`${baseStyles} bg-clr-white border-clr-black hover:bg-clr-black hover:text-clr-white ${extraStyles}`}
+      >
+        {text}
+      </button>
+    );
+  }
 }
 
-export function SimpleButton({ text, extraStyles, onClick, disabled, type, id }) {
-    if (disabled) {
-        return (
-            <>
-                <button className={`underline text-clr-white hover:text-clr-green-dark transition-all duration-300 ${extraStyles}`}>{text}</button>
-            </>
-        );
-    } else {
-        return (
-            <>
-                <button id={id} type={type} onClick={onClick} className={`underline text-clr-white hover:text-clr-green-dark transition-all duration-300 ${extraStyles}`}>{text}</button>
-            </>
-        );
-    }
+export function SimpleButton({
+  text,
+  asLink,
+  extraStyles,
+  onClick,
+  disabled,
+  type,
+  id,
+  link
+}) {
+  if (disabled) {
+    return (
+      <button
+        className={`underline text-clr-white hover:text-clr-green-dark transition-all duration-300 ${extraStyles}`}
+      >
+        {text}
+      </button>
+    );
+  } else if (asLink) {
+    return (
+      <a
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={`underline text-clr-white hover:text-clr-green-dark transition-all duration-300 ${extraStyles}`}
+      >
+        {text}
+      </a>
+    );
+  } else {
+    return (
+      <button
+        id={id}
+        type={type}
+        onClick={onClick}
+        className={`underline text-clr-white hover:text-clr-green-dark transition-all duration-300 ${extraStyles}`}
+      >
+        {text}
+      </button>
+    );
+  }
 }
 
 MainButton.propTypes = {
-    text: PropTypes.string.isRequired,
-    onClick: PropTypes.func,
-    disabled: PropTypes.bool,
-    type: PropTypes.string,
-    id: PropTypes.string,
-    extraStyles: PropTypes.string
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  type: PropTypes.string,
+  id: PropTypes.string,
+  extraStyles: PropTypes.string,
+  asLink: PropTypes.bool,
+  link: PropTypes.string,
 };
 
 MainButton.defaultProps = {
-    text: 'Button',
-    onClick: () => {},
+  text: "Button",
+  onClick: () => {},
 };
 
 SecondaryButton.propTypes = {
-    text: PropTypes.string.isRequired,
-    onClick: PropTypes.func,
-    disabled: PropTypes.bool,
-    type: PropTypes.string,
-    id: PropTypes.string,
-    extraStyles: PropTypes.string
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  type: PropTypes.string,
+  id: PropTypes.string,
+  extraStyles: PropTypes.string,
+  asLink: PropTypes.bool,
+  link: PropTypes.string,
 };
 
 SecondaryButton.defaultProps = {
-    text: 'Button',
-    onClick: () => {},
+  text: "Button",
+  onClick: () => {},
 };
 
 SecondaryButtonOutline.propTypes = {
-    text: PropTypes.string.isRequired,
-    onClick: PropTypes.func,
-    disabled: PropTypes.bool,
-    type: PropTypes.string,
-    id: PropTypes.string,
-    extraStyles: PropTypes.string
-}
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  type: PropTypes.string,
+  id: PropTypes.string,
+  extraStyles: PropTypes.string,
+  asLink: PropTypes.bool,
+  link: PropTypes.string,
+};
 
 SecondaryButtonOutline.defaultProps = {
-    text: 'Button',
-    onClick: () => {},
-}
+  text: "Button",
+  onClick: () => {},
+};
+
+SimpleButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  type: PropTypes.string,
+  id: PropTypes.string,
+  extraStyles: PropTypes.string,
+  asLink: PropTypes.bool,
+  link: PropTypes.string,
+};
+
+SimpleButton.defaultProps = {
+  text: "Button",
+  onClick: () => {},
+};
