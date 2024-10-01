e71492a176e9efa28ffce4875038eb51ae309b62
freddy74 <freddygarro74@gmail.com>
2024-10-01 13:28:34 -0600

    refactor: Update Loading_Screen component and CSS

--- src/components/ui/Loading_Screen.jsx
+++ src/components/ui/Loading_Screen.jsx
@@ -1,31 +1,40 @@
-import { useState, useEffect } from 'react';
-
+import { useState, useEffect } from "react";
+import "../../../public/css/Loading_Screen.css";
 export function Loading_Screen({ Loading }) {
-    const [isLoading, setIsLoading] = useState(true);
-    const [isAnimating, setIsAnimating] = useState(true);
-    const [isLoaded, setIsLoaded] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAnimating, setIsAnimating] = useState(true);
+  const [isLoaded, setIsLoaded] = useState(false);
 
-    useEffect(() => {
-        setIsLoading(Loading);
-        if (Loading === false) {
-            setTimeout(() => {
-                setIsAnimating(false);
-            }, 450);
-            setTimeout(() => {
-                setIsLoaded(true);
-            }, 470);
-        }
-    }, [Loading]);
+  useEffect(() => {
+    setIsLoading(Loading);
+    if (Loading === false) {
+      setTimeout(() => {
+        setIsAnimating(false);
+      }, 450);
+      setTimeout(() => {
+        setIsLoaded(true);
+      }, 470);
+    }
+  }, [Loading]);
 
-    return (
-        <>
-            {!isLoaded && (
-                <div className={`fixed inset-0 z-10 bg-clr-white h-screen flex items-center justify-center transition-all duration-200 delay-200 ${isLoading ? 'opacity-100' : 'opacity-0'}`}>
-                    <div className={`fixed inset-0 z-10 bg-clr-white h-screen flex items-center justify-center ${isAnimating ? 'flex' : 'hidden'}`}>
-                        <h1 className='font-semibold'>Cargando</h1>
-                    </div>
-                </div>
-            )}
-        </>
-    )
-}  
+  return (
+    <>
+      {!isLoaded && (
+        <div
+          className={`fixed inset-0 z-10 bg-clr-white h-screen flex items-center justify-center transition-all duration-200 delay-200 ${
+            isLoading ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          <div
+            className={`fixed inset-0 z-10 bg-clr-white h-screen flex items-center justify-center ${
+              isAnimating ? "flex" : "hidden"
+            }`}
+          >
+            
+            <div className="loader font-clash"></div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
