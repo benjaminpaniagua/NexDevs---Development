351f0f4b0e62100d6a3c5c1108fef1bbce091c30
LuisCarlosSolis <168393621+LuisCarlosSolis@users.noreply.github.com>
2024-10-06 19:05:30 -0600

    feat: Agregada la funcionalidad de editar correctamente el perfil

--- src/components/pages/WorkUserEdit.jsx
+++ src/components/pages/WorkUserEdit.jsx
@@ -7,12 +7,15 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import { FormInput, FormSelect, FormTextArea } from '../ui/FormInput.jsx';
 import { MainButton, SecondaryButton, SecondaryButtonOutline } from '../ui/Buttons';
+import { useEditWorkProfile } from '../../hooks/EditProfile/useEditWorkProfile.js';
+import { Link } from 'react-router-dom';
 
 export function WorkUserEdit() {
     const navigate = useNavigate();
     //const [isOwner, setIsOwner] = useState(false);
     const { userData, loading, error } = useFetchWorkUserData();
     const { userId } = useParams();
+    const { editProfile, editError } = useEditWorkProfile();
 
     const [previewImage, setPreviewImage] = useState(null);
     const defaultImage = '/images/default_profile_picture.jpg';
@@ -98,8 +101,8 @@ export function WorkUserEdit() {
     useEffect(() => {
         if (!loading) {
             if (userData.workId == userId) {
-                console.log("Is owner");
-                console.log(userData);
+                //console.log("Is owner");
+                //console.log(userData);
                 setFormData({
                     workId: userData.workId,
                     name: userData.name,
@@ -114,16 +117,26 @@ export function WorkUserEdit() {
                 });
                 setSelectedProvince(userData.province);
                 setAvailableCities(cityOptions[userData.province] || []);
-            }else if (userData.workId !== userId) {
-                window.location.href = '/WorkUserEdit/' + userData.workId;                
+            } else if (userData.workId !== userId) {
+                window.location.href = '/WorkUserEdit/' + userData.workId;
             }
         }
     }, [userData, loading]);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         console.log("submitted data", formData);
+
+        const result = await editProfile(formData);
+        if (result.success) {
+            console.log('Perfil editado con éxito', result);
+            window.location.href = '/workprofile/' + userData.workId;
+        } else if (editError) {
+            console.error('Error al editar el perfil', editError);
+        }
     };
+
+    /* className='bg-[url("/logo/Logo_BG.svg")] bg-repeat bg-[length:150px_150px]'*/
 
     return (
         <>
@@ -131,54 +144,61 @@ export function WorkUserEdit() {
             <Loading_Screen Loading={loading} />
             {/* Loading Screen */}
 
-            <form className='flex flex-col mx-auto px-20 max-w-[100rem] md:px-10 h-auto' onSubmit={handleSubmit}>
+            <div>
 
-                {/* Profile Picture */}
-                <div className="relative mt-14 mx-auto">
-                    <img
-                        src={previewImage || defaultImage}
-                        alt="Foto de perfil"
-                        className="w-56 h-56 rounded-full border-4 border-white object-cover"
-                    />
+                <form className='flex flex-col mx-auto px-20 max-w-[100rem] md:px-10 h-auto' onSubmit={handleSubmit}>
+                    {/* Profile Picture */}
+                    <div className="relative mt-14 mx-auto">
+                        <img
+                            src={previewImage || defaultImage}
+                            alt="Foto de perfil"
+                            className="w-56 h-56 rounded-full border-4 border-white object-cover"
+                        />
 
-                    {/* Botón para subir imagen */}
-                    <label htmlFor="profileImage" className="absolute bottom-0 transform translate-x-2 translate-y-2 w-10 h-10 bg-black rounded-full flex items-center justify-center cursor-pointer">
-                        <AddIcon style={{ color: 'white' }} />
-                    </label>
-                    <input
-                        id="profileImage"
-                        type="file"
-                        accept="image/*"
-                        onChange={handleImageChange}
-                        className="hidden"
-                    />
-                </div>
-                {/* Profile Picture */}
+                        {/* Botón para subir imagen */}
+                        <label htmlFor="profileImage" className="absolute bottom-0 transform translate-x-2 translate-y-2 w-10 h-10 bg-black rounded-full flex items-center justify-center cursor-pointer">
+                            <AddIcon style={{ color: 'white' }} />
+                        </label>
+                        <input
+                            id="profileImage"
+                            type="file"
+                            accept="image/*"
+                            onChange={handleImageChange}
+                            className="hidden"
+                        />
+                    </div>
+                    {/* Profile Picture */}
 
-                <div className='flex justify-center gap-4'>
-                    {/* Profile Info */}
-                    <div className="flex flex-col w-1/3 mt-10 gap-2">
-                        <FormInput id="company_name" type="text" name="name" title="Nombre de la Empresa" minLength={0} value={formData.name} onChange={handleInputChange} className="border h-12 bg-clr-white border-black rounded p-1" />
-                        <FormInput id="company_email" type="email" name="email" title="Email" minLength={0} value={formData.email} onChange={handleInputChange} className="border h-12 bg-clr-white border-black rounded p-1" />
-                        <FormInput id="company_number" type="text" name="number" value={formData.number} title="Numero de celular" onChange={handleNumberChange} minLength={8} maxLength={8} className="border h-12 bg-clr-white border-black rounded p-1" />
+                    <div className='flex justify-center gap-4'>
+                        {/* Profile Info */}
+                        <div className="flex flex-col w-1/3 mt-10 gap-2">
+                            <FormInput id="company_name" type="text" name="name" title="Nombre de la Empresa" minLength={0} value={formData.name} onChange={handleInputChange} className="border h-12 bg-clr-white border-black rounded p-1" />
+                            <FormInput id="company_email" type="email" name="email" title="Email" minLength={0} value={formData.email} onChange={handleInputChange} className="border h-12 bg-clr-white border-black rounded p-1" />
+                            <FormInput id="company_number" type="text" name="number" value={formData.number} title="Numero de celular" onChange={handleNumberChange} minLength={8} maxLength={8} className="border h-12 bg-clr-white border-black rounded p-1" />
+                        </div>
+
+                        <div className="flex flex-col mt-10 w-1/3 gap-2">
+                            <FormSelect id="company_state" name="province" title="Provincia" value={formData.province} onChange={handleSelectChange} options={stateOptions} className="border h-12 bg-clr-white border-black rounded p-1" />
+                            <FormSelect id="company_city" name="city" title="Ciudad" value={formData.city} onChange={handleSelectChange} options={availableCities} className="border h-12 bg-clr-white border-black rounded p-1" />
+                        </div>
+                        {/* Profile Info */}
                     </div>
 
-                    <div className="flex flex-col mt-10 w-1/3 gap-2">
-                    <FormSelect id="company_state" name="province" title="Provincia" value={formData.province} onChange={handleSelectChange} options={stateOptions} className="border h-12 bg-clr-white border-black rounded p-1" />
-                    <FormSelect id="company_city" name="city" title="Ciudad" value={formData.city} onChange={handleSelectChange} options={availableCities} className="border h-12 bg-clr-white border-black rounded p-1" />
+                    <div className="flex flex-col w-1/2 mx-auto mt-5">
+                        <FormTextArea id="company_description" name="workDescription" title="Descripción" minLength={0} maxLength={450} value={formData.workDescription} onChange={handleInputChange} className="border h-44 md:h-32 bg-clr-white border-black rounded p-1" />
                     </div>
-                    {/* Profile Info */}
-                </div>
 
-                <div className="flex flex-col w-1/2 mx-auto mt-5">
-                    <FormTextArea id="company_description" name="workDescription" title="Descripción" minLength={0} maxLength={450} value={formData.workDescription} onChange={handleInputChange} className="border h-44 md:h-32 bg-clr-white border-black rounded p-1" />
-                </div>
+                    {editError && <p className="text-red-500 flex justify-center mt-2">{editError}</p>}
 
-                <div className='mx-auto flex gap-5 mb-5'>
-                    <MainButton id="save" type="BUTTON" text="Regresar" extraStyles="mt-8 p-2" />
-                    <MainButton id="save" type="submit" text="Guardar" extraStyles="mt-8 p-2" />
-                </div>
-            </form>
+                    <div className='mx-auto flex gap-5 mb-5'>
+                        <Link to={'/workprofile/' + userData.workId}>
+                            <MainButton id="back" type="button" text="Regresar" extraStyles="mt-8 p-2" />
+                        </Link>
+                        <MainButton id="save" type="submit" text="Guardar" extraStyles="mt-8 p-2" />
+                    </div>
+                </form>
+            </div>
+
         </>
     )
 }  
--- src/components/ui/Modal_Profile/Modal_Profile.jsx
+++ src/components/ui/Modal_Profile/Modal_Profile.jsx
@@ -31,11 +31,11 @@ export function Modal_Profile({ onClose }) {
                         {/* Botón de cerrar */}
                         <svg className="h-full w-full fill-clr-blue opacity-20">
                             <defs>
-                                <pattern id="backgroundPattern" x="0" y="0" width="98" height="98" patternUnits="userSpaceOnUse" patternTransform="translate(24 55)">
+                                <pattern id="backgroundPattern1" x="0" y="0" width="98" height="98" patternUnits="userSpaceOnUse" patternTransform="translate(24 55)">
                                     <path transform="scale(0.45)" d="m112.4,17.77c-15.09,15.09-21.09,36.54-16.04,57.38,4.09,16.9-.15,34.77-11.4,48.05-1,1.17-2.02,2.28-3.09,3.36-11.14,11.14-26.6,16.98-42.43,15.87-10.69-.75-21.14,3.14-28.66,10.66-14.34,14.35-14.38,37.65-.08,51.95,14.3,14.3,37.58,14.25,51.92-.08,7.53-7.53,11.41-17.97,10.66-28.66-1.11-15.83,4.74-31.3,15.87-42.43,1.07-1.07,2.18-2.1,3.36-3.09,13.31-11.26,31.16-15.48,48.05-11.4,20.85,5.06,42.31-.95,57.4-16.04,23.66-23.66,23.72-62.1.13-85.68s-62.02-23.54-85.68.12h0Z" />
                                 </pattern>
                             </defs>
-                            <rect x="0" y="0" width="100%" height="100%" fill="url(#backgroundPattern)" />
+                            <rect x="0" y="0" width="100%" height="100%" fill="url(#backgroundPattern1)" />
                         </svg>
                     </div>
                     {/* Upper Background */}
