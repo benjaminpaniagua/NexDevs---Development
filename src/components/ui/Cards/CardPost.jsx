c72869cea364cd2ad88ec35f186d04c1c1bcc37a
freddy74 <freddygarro74@gmail.com>
2024-10-27 22:23:43 -0600

    feat: add new icons for menu dots and trash

--- src/components/ui/Cards/CardPost.jsx
+++ src/components/ui/Cards/CardPost.jsx
@@ -1,4 +1,4 @@
-import{ useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { ICONS } from "../ICONS";
 import { SecondaryButton } from "../Buttons";
 import { FormInput } from "../FormInput";
@@ -6,7 +6,9 @@ import { useFetchWorkUserData } from "../../../hooks/useFetchWorkUserData";
 import { useCreateComments } from "../../../hooks/useCreateComments";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import { useDeletePost } from "../../../hooks/useDeletePost";
+import { useDeleteComment } from "../../../hooks/useDeleteComments";
 
 export function CardPost({
   postId,
@@ -22,13 +24,59 @@ export function CardPost({
 }) {
   const [showModal, setShowModal] = useState(false);
   const [comments, setComments] = useState([]);
+  const [postComment, setPostComment] = useState("");
+  const { createComments } = useCreateComments();
+
   const [currentLikesCount, setCurrentLikesCount] = useState(likesCount);
   const { userData } = useFetchWorkUserData();
-  const { createComments } = useCreateComments();
-  const [postComment, setPostComment] = useState("");
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isLiked, setIsLiked] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isAuthor, setIsAuthor] = useState(false); // Estado para verificar si el usuario es el autor del post o no
+  const [isWorker, setIsWorker] = useState(false); // Estado para verificar si el usuario es un trabajador o no
+  const [isUser, setIsUser] = useState(false); // Estado para verificar si el usuario es un usuario o no
+  // const [deleteComment] = useDeleteComment(); // Asegúrate de que esto se usa en el cuerpo del componente
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (userData) {
+      if (userData.profileType === "W") {
+        setIsWorker(true);
+      }
+      if (userData.profileType === "U") {
+        setIsUser(true);
+      }
+      if (userData.profileType === "W" && userData.workId === workId) {
+        setIsAuthor(true);
+      }
+      if (userData.profileType === "U" && userData.userId === userId) {
+        setIsAuthor(true);
+      }
+    }
+  }, [userData]);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
+  const { deletePost, loading, error } = useDeletePost();
+
+  const handlePostDelete = async () => {
+    const confirmation = window.confirm(
+      "¿Estás seguro de que deseas eliminar este post?"
+    );
+    if (confirmation) {
+      const result = await deletePost(postId);
+      if (!error) {
+        // Aquí puedes manejar la UI, como remover el post eliminado de la lista
+        alert(result); // Mensaje de éxito
+      } else {
+        alert(`Error: ${error}`); // Manejo de error
+      }
+    }
+  };
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -46,12 +94,12 @@ export function CardPost({
       const workProfileId = userData?.workId || 0; // Obtenemos workProfileId desde userData
 
       // Validar si ambos son null, no hacer la solicitud
-      if (!userId && !workProfileId) {
-        console.error(
-          "No se ha encontrado userId ni workProfileId para verificar el like."
-        );
-        return;
-      }
+      // if (!userId && !workProfileId) {
+      //   console.error(
+      //     "No se ha encontrado userId ni workProfileId para verificar el like."
+      //   );
+      //   return;
+      // }
 
       // Construimos la URL con los parámetros correctos
       const url = `https://localhost:7038/Likes/CheckIfIsLiked?postId=${postId}&userId=${userId}&workProfileId=${workProfileId}`;
@@ -66,14 +114,14 @@ export function CardPost({
       if (response.ok) {
         const data = await response.json();
         setIsLiked(data); // La respuesta es booleana, actualiza el estado de isLiked
-        console.log("isLiked después de la actualización:", data);
+        // console.log("isLiked después de la actualización:", data);
       } else {
-        console.error("Error al verificar si el post ha sido liked");
+        // console.error("Error al verificar si el post ha sido liked");
       }
     } catch (error) {
-      console.error("Error al verificar si el post ha sido liked:", error);
+      // console.error("Error al verificar si el post ha sido liked:", error);
     }
-    console.log("isLiked", isLiked);
+    // console.log("isLiked", isLiked);
   };
 
   const handleLikePost = async () => {
@@ -296,21 +344,37 @@ export function CardPost({
     };
   }, [showModal]);
 
-  // const handleOutsideClick = (e) => {
-  //   if (e.target.className.includes("modal-overlay")) {
-  //     closeModal();
-  //   }
-  // };
+  const handleOutsideClick = (e) => {
+    if (e.target.classList.contains("fixed")) {
+      closeModal();
+    }
+  };
+
+  const handleCommentDelete = async (commentId) => {
+    fetch = async () => {
+      const response = await fetch(
+        `https://localhost:7038/Comments/Eliminar?commentId=${commentId}`,
+        {
+          method: "DELETE",
+          headers: {
+            Accept: "text/plain",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Error al eliminar el comentario");
+      }
+
+      return true;
+    };
+  };
 
   return (
     <>
-      {/* Post Card */}
-      <div
-        className="h-auto flex flex-col gap-2 transition-all"
-        onClick={onClick}
-      >
+      <div className="h-auto flex flex-col gap-2 transition-all">
         {/* Post Picture */}
-        <div className="rounded-lg overflow-hidden">
+        <div className="relative rounded-lg overflow-hidden">
           <img
             className="aspect-[4/3] w-full h-full object-cover"
             src={
@@ -320,10 +384,48 @@ export function CardPost({
             }
             alt="Post_Image"
           />
+          {/* Dropdown Menu */}
+          {/* Menu Button */}
+          {isAuthor && (
+            <button
+              onClick={toggleMenu}
+              className="transition-all duration-300 absolute top-2 right-2 bg-black/40 hover:bg-black/60 text-white rounded-full px-2 py-2"
+            >
+              {ICONS.menu_dots}
+              <span className="sr-only">Más opciones</span>
+            </button>
+          )}
+          {/* Menu */}
+          {isMenuOpen &&
+            isAuthor && ( // Asegúrate de que el menú solo aparezca si es el autor
+              <div className="absolute right-2 top-10 bg-white border rounded shadow-lg w-40">
+                <div className="flex flex-col p-2">
+                  <button
+                    onClick={() => {
+                      // Lógica para editar
+                      console.log("Editar acción");
+                      setIsMenuOpen(false);
+                    }}
+                    className="flex items-center p-2 text-left hover:bg-gray-100"
+                  >
+                    Editar
+                  </button>
+                  <button
+                    onClick={handlePostDelete}
+                    disabled={loading}
+                    className={`flex items-center p-2 text-red-600 hover:bg-red-100 ${
+                      loading ? "cursor-not-allowed" : ""
+                    }`}
+                  >
+                    {loading ? "Eliminando..." : "Eliminar"}
+                  </button>
+                </div>
+              </div>
+            )}
         </div>
 
         {/* Content */}
-        <div className="flex flex-col ">
+        <div className="flex flex-col">
           {/* Post Title */}
           <h4>{title}</h4>
 
@@ -336,12 +438,13 @@ export function CardPost({
 
           {/* Bottom */}
           <div className="flex">
-            {/* Autor */}
+            {/* Author */}
             <Link to={`/workprofile/${workId}`}>
               <div className="flex cursor-pointer">
                 <img
                   src={
-                    profilePictureUrl === "ND" || profilePictureUrl === "default_image_url"
+                    profilePictureUrl === "ND" ||
+                    profilePictureUrl === "default_image_url"
                       ? "/images/default_profile_picture.jpg"
                       : profilePictureUrl
                   }
@@ -354,7 +457,6 @@ export function CardPost({
               </div>
             </Link>
             {/* Icons */}
-            {/* Íconos */}
             <section className="flex gap-3 xs:gap-2 items-center ml-auto">
               <div className="flex gap-1 items-center">
                 <button
@@ -368,7 +470,10 @@ export function CardPost({
                 </h4>
               </div>
               <div className="flex gap-1 items-center">
-                <button className="transition-all hover:scale-110" onClick={openModal}>
+                <button
+                  className="transition-all hover:scale-110"
+                  onClick={openModal}
+                >
                   {ICONS.comment}
                 </button>
                 <h4 className="text-clr-black font-bold">{commentsCount}</h4>
@@ -388,7 +493,7 @@ export function CardPost({
       {showModal && (
         <div
           className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-start sm:items-center justify-center z-50 overflow-y-auto"
-        // onClick={handleOutsideClick}
+          onClick={handleOutsideClick}
         >
           <div className="bg-white sm:p-4 p-6 rounded-lg m-4 max-w-5xl w-full relative h-fit">
             <div className="flex justify-between items-center w-full pb-4 bg-white sticky top-[-16px] z-10">
@@ -397,7 +502,8 @@ export function CardPost({
                   <div className="h-12 w-12 bg-gray-300 rounded-full flex items-center justify-center">
                     <img
                       src={
-                        profilePictureUrl === "ND" || profilePictureUrl === "default_image_url"
+                        profilePictureUrl === "ND" ||
+                        profilePictureUrl === "default_image_url"
                           ? "/images/default_profile_picture.jpg"
                           : profilePictureUrl
                       }
@@ -449,24 +555,46 @@ export function CardPost({
             <p>{description}</p>
             <div className="mt-4">
               <h4 className="font-bold">Comentarios:</h4>
+
               <div className="mt-2">
                 {comments.length > 0 ? (
                   comments
                     .sort((a, b) => new Date(b.createAt) - new Date(a.createAt))
                     .map((comment) => (
-                      <div
-                        key={comment.commentId}
-                        className="border-b border-gray-200 py-2"
-                      >
-                        {comment.workId && (
-                          <Link to={`/workprofile/${comment.workId}`}>
+                      <div className="flex justify-between">
+                        <div
+                          key={comment.commentId}
+                          className="border-b border-gray-200 py-2"
+                        >
+                          {comment.workId && (
+                            <Link to={`/workprofile/${comment.workId}`}>
+                              <div className="flex items-center">
+                                <img
+                                  src={
+                                    comment.profilePictureUrlWorker === "ND" ||
+                                    !comment.profilePictureUrlWorker
+                                      ? "/images/default_profile_picture.jpg"
+                                      : comment.profilePictureUrlWorker
+                                  }
+                                  alt="Foto de perfil"
+                                  className="w-8 h-8 rounded-full mr-2"
+                                />
+                                <h5 className="font-bold">
+                                  {comment.firstName}{" "}
+                                  {comment.lastName || comment.name || ""}
+                                </h5>
+                              </div>
+                            </Link>
+                          )}
+
+                          {comment.userId && (
                             <div className="flex items-center">
                               <img
                                 src={
-                                  comment.profilePictureUrlWorker === "ND" ||
-                                    !comment.profilePictureUrlWorker
-                                    ? "/images/default_profile_picture.jpg"
-                                    : comment.profilePictureUrlWorker
+                                  comment.profilePictureUrlUser === "ND" ||
+                                  !comment.profilePictureUrlUser
+                                    ? "/images/Profile_Placeholder.png"
+                                    : comment.profilePictureUrlUser
                                 }
                                 alt="Foto de perfil"
                                 className="w-8 h-8 rounded-full mr-2"
@@ -476,28 +604,15 @@ export function CardPost({
                                 {comment.lastName || comment.name || ""}
                               </h5>
                             </div>
-                          </Link>
-                        )}
-
-                        {comment.userId && (
-                          <div className="flex items-center">
-                            <img
-                              src={
-                                comment.profilePictureUrlUser === "ND" ||
-                                  !comment.profilePictureUrlUser
-                                  ? "/images/Profile_Placeholder.png"
-                                  : comment.profilePictureUrlUser
-                              }
-                              alt="Foto de perfil"
-                              className="w-8 h-8 rounded-full mr-2"
-                            />
-                            <h5 className="font-bold">
-                              {comment.firstName}{" "}
-                              {comment.lastName || comment.name || ""}
-                            </h5>
-                          </div>
-                        )}
-                        <p>{comment.contentComment}</p>
+                          )}
+                          <p>{comment.contentComment}</p>
+                        </div>
+                        <div>
+                          <button onClick={handleCommentDelete}>
+                            {ICONS.trash}
+                            {/* <span className="sr-only">Más opciones</span> */}
+                          </button>
+                        </div>
                       </div>
                     ))
                 ) : (
