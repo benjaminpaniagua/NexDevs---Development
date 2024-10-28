3d971856eae1ae2f6bcd2725613890a53156da89
freddy74 <freddygarro74@gmail.com>
2024-10-27 22:23:11 -0600

    feat: add useDeletePosts and useDeleteComments

---
+++ src/hooks/useDeleteComments.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+export const useDeleteComment = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const deleteComment = async (commentId) => {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const response = await fetch(`https://localhost:7038/Comments/Eliminar?commentId=${commentId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Accept': 'text/plain',
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error('Error al eliminar el comentario');
+            }
+
+            // No se espera contenido en la respuesta, solo un código 200
+            return true; // Retorna true para indicar éxito
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return { deleteComment, loading, error };
+};
---
+++ src/hooks/useDeletePost.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { useFetchPosts } from './useFetchPosts';
+
+export const useDeletePost = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    
+    const deletePost = async (postId) => {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const response = await fetch(`https://localhost:7038/Posts/Eliminar?postId=${postId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Accept': 'text/plain',
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error('Error al eliminar el post');
+            }
+
+            const result = await response.text();
+            console.log(result); // Para verificar la respuesta del servidor
+            return result; // Puedes retornar el resultado o manejarlo según lo necesites
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return { deletePost, loading, error };
+};
