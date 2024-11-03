80b2346d96402a9063c11a49f711b36a99b833ec
freddy74 <freddygarro74@gmail.com>
2024-11-03 00:04:25 -0600

    feat: enhance NavBar layout for improved responsiveness and structure

--- src/components/ui/NavBar/NavBar.jsx
+++ src/components/ui/NavBar/NavBar.jsx
@@ -110,7 +110,7 @@ const NavBar = () => {
         <Link to="/Access_Panel/login">
           <SecondaryButton
             text="Únete"
-            extraStyles={"px-4 py-2 text-fs-med font-bold"}
+            extraStyles={"px-4 py-2 w-full text-fs-med font-bold"}
           />
         </Link>
       );
@@ -149,66 +149,68 @@ const NavBar = () => {
   return (
     <>
       <nav
-        className={`fixed top-0 left-0 w-full flex items-center justify-between p-5 z-20 transition-all duration-300 shadow-lg bg-clr-white ${
+        className={`fixed top-0 left-0 w-full flex items-center justify-between py-5 z-20 transition-all duration-300 shadow-lg bg-clr-white ${
           isScrolled ? "-translate-y-36" : "translate-y-0"
         }`}
       >
-        <div className="w-full max-w-[100rem] flex items-center justify-between px-20 mx-auto ">
-          <div className="flex items-center">
-            <Link to="/">
-              <img src="/logo/Logo.svg" alt="Logo" className="h-12 sm:h-9" />
-            </Link>
-          </div>
-          <div className="w-full flex justify-end items-center gap-4 tracking-wide font-medium sm:hidden">
-            <NavLinks links={links} />
-            {renderProfilePicture()}
-          </div>
-        </div>
-
-        <div className="sm:flex items-center gap-10 hidden">
-          {renderProfilePictureMovil()}
-          <label className="flex-col gap-2 w-8 sm:flex hidden">
-            <input
-              id="menu-toggle"
-              className="peer hidden"
-              type="checkbox"
-              onClick={toggleMenu}
-            />
-            <div className="rounded-2xl h-[3px] w-1/2 bg-clr-black duration-500 peer-checked:rotate-[225deg] origin-right peer-checked:-translate-x-[12px] peer-checked:-translate-y-[1px]"></div>
-            <div className="rounded-2xl h-[3px] w-full bg-clr-black duration-500 peer-checked:-rotate-45"></div>
-            <div className="rounded-2xl h-[3px] w-1/2 bg-clr-black duration-500 place-self-end peer-checked:rotate-[225deg] origin-left peer-checked:translate-x-[12px] peer-checked:translate-y-[1px]"></div>
-          </label>
-        </div>
-
-        {isOpen && (
-          <div
-            className={`absolute top-full left-0 z-10 right-0 bg-clr-white p-4 sm:flex hidden md:justify-start md:items-start transition-all shadow-lg duration-300 ${
-              isMenuAnimating ? "animate-menu-open" : "animate-menu-close"
-            }`}
-          >
-            <NavLinks links={links} />
-          </div>
-        )}
-        {/* Modal de perfil */}
-        {isModalOpen && (
-          <div className={`absolute top-0 right-0 z-30 sm:m-auto`}>
-            {/* Fondo oscuro para el modal */}
-            <div
-              className={`fixed inset-0 h-screen transition-opacity duration-300 bg-black bg-opacity-50 ${
-                isAnimating ? "opacity-100" : "opacity-0"
-              }`}
-              onClick={handleCloseModal}
-            ></div>
-            {/* Modal de la esquina */}
-            <div
-              className={`mt-5 mr-5 w-[22rem] sm:mx-3 transform transition-transform duration-300 ease-out scale-0 origin-center ${
-                isAnimating ? "animate-modal-open" : "animate-modal-close"
-              }`}
-            >
-              <Modal_Profile onClose={handleCloseModal} />
+        <section className="w-full max-w-[100rem] flex items-center justify-between px-20 xs:px-6 md:px-10 m-auto">
+          <div className="w-full flex items-center justify-between ">
+            <div className="flex items-center">
+              <Link to="/">
+                <img src="/logo/Logo.svg" alt="Logo" className="h-12 sm:h-9" />
+              </Link>
+            </div>
+            <div className="w-full flex justify-end items-center gap-4 tracking-wide font-medium sm:hidden">
+              <NavLinks links={links} />
+              {renderProfilePicture()}
             </div>
           </div>
-        )}
+
+          <div className="w-full sm:flex items-center justify-end gap-10 hidden">
+            {renderProfilePictureMovil()}
+            <label className="flex-col gap-2 w-8 sm:flex hidden">
+              <input
+                id="menu-toggle"
+                className="peer hidden"
+                type="checkbox"
+                onClick={toggleMenu}
+              />
+              <div className="rounded-2xl h-[3px] w-1/2 bg-clr-black duration-500 peer-checked:rotate-[225deg] origin-right peer-checked:-translate-x-[12px] peer-checked:-translate-y-[1px]"></div>
+              <div className="rounded-2xl h-[3px] w-full bg-clr-black duration-500 peer-checked:-rotate-45"></div>
+              <div className="rounded-2xl h-[3px] w-1/2 bg-clr-black duration-500 place-self-end peer-checked:rotate-[225deg] origin-left peer-checked:translate-x-[12px] peer-checked:translate-y-[1px]"></div>
+            </label>
+          </div>
+
+          {isOpen && (
+            <div
+              className={`absolute top-full left-0 z-10 right-0 bg-clr-white p-4 sm:flex hidden md:justify-start md:items-start transition-all shadow-lg duration-300 ${
+                isMenuAnimating ? "animate-menu-open" : "animate-menu-close"
+              }`}
+            >
+              <NavLinks links={links} />
+            </div>
+          )}
+          {/* Modal de perfil */}
+          {isModalOpen && (
+            <div className={`absolute top-0 right-0 z-30 sm:m-auto`}>
+              {/* Fondo oscuro para el modal */}
+              <div
+                className={`fixed inset-0 h-screen transition-opacity duration-300 bg-black bg-opacity-50 ${
+                  isAnimating ? "opacity-100" : "opacity-0"
+                }`}
+                onClick={handleCloseModal}
+              ></div>
+              {/* Modal de la esquina */}
+              <div
+                className={`mt-5 mr-5 w-[22rem] sm:mx-3 transform transition-transform duration-300 ease-out scale-0 origin-center ${
+                  isAnimating ? "animate-modal-open" : "animate-modal-close"
+                }`}
+              >
+                <Modal_Profile onClose={handleCloseModal} />
+              </div>
+            </div>
+          )}
+        </section>
       </nav>
     </>
   );
